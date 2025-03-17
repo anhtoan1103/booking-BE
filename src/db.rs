b331d0db4206f4c75feb3b8df7cdb971a27976de
anhtoan1103 <toantra732@gmail.com>
2025-03-17 22:21:57 +0700

    add loop and try catch to db connection

--- src/db.rs
+++ src/db.rs
@@ -1,1 +1,28 @@
+use dotenv::dotenv;
+use sqlx::pool::Pool;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::Postgres;
+use std::env;
+// use sqlx::mysql::MySqlPoolOptions;
+// etc.
 
+pub async fn connect_db() -> Result<Pool<Postgres>, sqlx::Error> {
+    dotenv().ok();
+    let database_url: String = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    println!("Kết nối database: {}", database_url);
+
+    match PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+    {
+        Ok(p) => {
+            print!("connected");
+            Ok(p)
+        }
+        Err(e) => {
+            print!("cannot connect");
+            Err(e)
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,23 @@
-use dotenv::dotenv;
-use sqlx::postgres::PgPoolOptions;
-use std::env;
+use tokio::time::sleep;
+use tokio::time::Duration;
+
+mod db;
 // use sqlx::mysql::MySqlPoolOptions;
 // etc.
 
 // or #[tokio::main]
 // or #[actix_web::main]
 #[tokio::main] // Requires the `attributes` feature of `async-std`
-async fn main() -> Result<(), sqlx::Error> {
-    dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    println!("Kết nối database: {}", database_url);
-
-    // Create a connection pool
-    //  for MySQL/MariaDB, use MySqlPoolOptions::new()
-    //  for SQLite, use SqlitePoolOptions::new()
-    //  etc.
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect(&database_url)
-        .await?;
-
-    // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL/MariaDB)
-    let row: (i64,) = sqlx::query_as("SELECT $1")
-        .bind(150_i64)
-        .fetch_one(&pool)
-        .await?;
-
-    assert_eq!(row.0, 50);
-
-    Ok(())
+async fn main() {
+    loop {
+        match db::connect_db().await {
+            Ok(..) => {
+                break;
+            }
+            Err(error) => {
+                eprintln!("cannot connect to db {:?}", error);
+                sleep(Duration::from_millis(1000)).await;
+            }
+        }
+    }
 }
